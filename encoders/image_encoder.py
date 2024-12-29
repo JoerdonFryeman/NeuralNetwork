baed36ac5a386469cff035ad14ef55d2e746e401
Joerdon Fryeman <126917322+JoerdonFryeman@users.noreply.github.com>
2024-12-29 16:39:47 +0100

    20. Небольшие исправления кода, рефакторинг и написание документации.
    Данные изменения не затрагивают функциональность программы и носят лишь косметический характер.

--- encoders/image_encoder.py
+++ encoders/image_encoder.py
@@ -11,30 +11,34 @@ def encode_image_to_array(file_path: str, invert_colors: bool = False, size: tup
     :param file_path: Путь к файлу изображения.
     :param invert_colors: Булевая переменная для инверсии цветов (черный <-> белый).
     :param size: Размер для изменения изображения.
+
     :return: Нормализованный одномерный список пикселей изображения.
     """
     with Image.open(file_path) as img:
-        img = img.convert("L")  # Конвертация в градации серого
-        img = img.resize(size)  # Изменение размера изображения на заданное количество пикселей
+        # Конвертация в градации серого.
+        img = img.convert("L")
+        # Изменение размера изображения на заданное количество пикселей.
+        img = img.resize(size)
         img_array = np.array(img)
-        normalized_array = (img_array / 255.0).flatten().tolist()  # Нормализация и преобразование в одномерный список
+        # Нормализация и преобразование в одномерный список.
+        normalized_array = (img_array / 255.0).flatten().tolist()
         if invert_colors:
-            # Инвертирование значения
+            # Инвертирование значения.
             normalized_array = [1 - pixel for pixel in normalized_array]
         return normalized_array
 
 
 def encode_images_from_directory(dir_path: str, output_file: str, invert_colors: bool = False, size: tuple = (7, 7)):
     """
-    Преобразует изображения из директории и поддиректорий в массив данных и сохраняет в JSON файл.
+    Преобразует изображения из директории и поддиректорий в массив данных и сохраняет в json файл.
 
     :param dir_path: Путь к главной директории с поддиректориями.
-    :param output_file: Путь к выходному файлу JSON.
+    :param output_file: Путь к выходному файлу json.
     :param invert_colors: Булевая переменная для инверсии цветов (черный <-> белый).
     :param size: Размер для изменения изображения.
     """
     if not os.path.isdir(dir_path):
-        raise ValueError(f"Directory {dir_path} does not exist")
+        raise ValueError(f'Каталог {dir_path} не существует!')
     numbers_data = {}
     for sub_dir_name in sorted(os.listdir(dir_path)):
         sub_dir_path = os.path.join(dir_path, sub_dir_name)
@@ -48,11 +52,11 @@ def encode_images_from_directory(dir_path: str, output_file: str, invert_colors:
                     layer = encode_image_to_array(file_path, invert_colors, size)
                     images_data.append(layer)
                 except Exception as e:
-                    print(f"Failed to process {file_name}: {e}")
+                    print(f'Не удалось обработать {file_name}: {e}')
             else:
-                raise FileNotFoundError(f"Expected file {file_name} does not exist in the directory {sub_dir_path}")
+                raise FileNotFoundError(f'Ожидаемый файл {file_name} не существует в каталоге {sub_dir_path}!')
         numbers_data[sub_dir_name] = images_data
-    # Приведение структуры к формату с ключом "numbers"
-    data = {"numbers": numbers_data}
+    # Приведение структуры к формату с ключом 'numbers'.
+    data = {'numbers': numbers_data}
     with open(output_file, 'w') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
